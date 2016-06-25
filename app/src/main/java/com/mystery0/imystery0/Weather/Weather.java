6571415e7ae282d32a7710f7a91b65e8e8d0b2ac
Mystery00 <mystery0dyl520@gmail.com>
2016-06-25 20:59:14 +0800

    new iWeather

---
+++ app/src/main/java/com/mystery0/imystery0/Weather/Weather.java
@@ -0,0 +1,17 @@
+package com.mystery0.imystery0.Weather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mystery0.imystery0.R;
+
+public class Weather extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
