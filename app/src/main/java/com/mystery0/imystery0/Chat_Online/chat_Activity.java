ac0b88b2670e50afedf2014156a45814bd6747ae
Mystery00 <mystery0dyl520@gmail.com>
2016-06-27 13:23:46 +0800

    new chat

--- app/src/main/java/com/mystery0/imystery0/Chat_Online/chat_Activity.java
+++ app/src/main/java/com/mystery0/imystery0/Chat_Online/chat_Activity.java
@@ -2,16 +2,30 @@ package com.mystery0.imystery0.Chat_Online;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 import com.mystery0.imystery0.R;
 
 public class chat_Activity extends AppCompatActivity
 {
-
+    private ListView listView;
+    private EditText text;
+    private ImageButton sendButton;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_layout);
+
+        initialization();
+    }
+
+    private void initialization()
+    {
+        listView = (ListView) findViewById(R.id.listView);
+        text = (EditText) findViewById(R.id.chat_text);
+        sendButton = (ImageButton) findViewById(R.id.send);
     }
 }
