6794eca6353a1ebca65fe404dbbd8aa6355f7a7a
Mystery00 <mystery0dyl520@gmail.com>
2016-07-11 13:56:18 +0800

    personal net disc

---
+++ app/src/main/java/com/mystery0/imystery0/PublicMethod/HeadFile.java
@@ -0,0 +1,19 @@
+package com.mystery0.imystery0.PublicMethod;
+
+/**
+ * Created by myste on 2016-7-11-0011.
+ * 头像类的上传与下载
+ */
+public class HeadFile
+{
+    public static void getHead(String username)
+    {
+
+    }
+
+    public static boolean setHead(String path)
+    {
+
+        return true;
+    }
+}
